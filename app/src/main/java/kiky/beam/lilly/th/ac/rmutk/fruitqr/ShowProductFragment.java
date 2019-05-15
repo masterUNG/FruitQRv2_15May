07abef9dbc50733efd5a2c073e99317e85400408
masterUNG <phrombutr@gmail.com>
2019-05-15 15:59:07 +0700

    Finish 15May

--- app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/ShowProductFragment.java
+++ app/src/main/java/kiky/beam/lilly/th/ac/rmutk/fruitqr/ShowProductFragment.java
@@ -16,6 +16,7 @@ import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -68,20 +69,6 @@ public class ShowProductFragment extends Fragment {
 
         String[] columnDetailProduct = myconstant.getColumnDetailProduct(); //ดึงค่าจาก myconstane
 
-//        try {
-//
-//            GetDataWhereOneColumn getDataWhereOneColumn = new GetDataWhereOneColumn(getActivity());
-//            getDataWhereOneColumn.execute("id", idProduct, myconstant.getUrlGetProductWhereId());
-//            String jsonProduct = getDataWhereOneColumn.get();
-//            Log.d("18AprilV3", "jsonProduct ==>>> " + jsonProduct);
-//
-//            JSONArray jsonArray = new JSONArray(jsonProduct);
-//            JSONObject jsonObject = jsonArray.getJSONObject(0);
-//            for (int i = 0; i<columnDetailProduct.length; i+= 1){ //นับคอลัม
-//                productStringArrayList.add(jsonObject.getString(columnDetailProduct[i])); //ตำแหน่ง i
-//                Log.d("18ApriV3", "productStringArrayList[" + i + "] ==> " + productStringArrayList.get(i));
-//
-//            }
 
         try {
 
@@ -114,6 +101,26 @@ public class ShowProductFragment extends Fragment {
                 Log.d("18AprilV4", "framerStringArrayList[" + i + "] ==> " + framerStringArrayList.get(i));
             }
 
+
+//             For Farmer
+//            GetDataWhereOneColumn farmerGetDataWhereOneColumn2 = new GetDataWhereOneColumn(getActivity());
+//            farmerGetDataWhereOneColumn2.execute("id", framerStringArrayList.get(1), myconstant.getUrlGetUserWhereId());
+//            String result = farmerGetDataWhereOneColumn2.get();
+//            Log.d("15MayV1", "result ==> " + result);
+//            JSONArray farmerJsonArray2 = new JSONArray(result);
+//            JSONObject farmerJsonObject2 = farmerJsonArray2.getJSONObject(0);
+//
+//            TextView nameFarmerTextView = getView().findViewById(R.id.txtFirstName);
+//            nameFarmerTextView.setText(farmerJsonObject2.getString("Name"));
+//
+//            TextView addFarmerTextView = getView().findViewById(R.id.txtFruitAdd);
+//            addFarmerTextView.setText(farmerJsonObject2.getString("Address"));
+//
+//            TextView phoneFarmerTextView = getView().findViewById(R.id.txtFruitPhone);
+//            phoneFarmerTextView.setText(farmerJsonObject2.getString("Phone"));
+
+
+
             //สำหรับ User
             GetDataWhereOneColumn getDataWhereOneColumn2 = new GetDataWhereOneColumn(getActivity());
             getDataWhereOneColumn2.execute("id", productStringArrayList.get(1),myconstant.getUrlGetUserWhereId());
@@ -130,19 +137,7 @@ public class ShowProductFragment extends Fragment {
 
 
 
-//            //สำหรับ Framer
-//            GetDataWhereOneColumn getDataWhereOneColumn3 = new GetDataWhereOneColumn(getActivity());
-//            getDataWhereOneColumn3.execute("id", framerStringArrayList.get(1), myconstant.getUrlGetUserWhereId()); //อยู่ในตำแหน่งที่ 1
-//            String jsonUserFramer = getDataWhereOneColumn3.get();
-//            Log.d("28AprilV5", "jsonFramer ==>>> " + jsonUserFramer);
-//            String[] columnUserFarmer = myconstant.getColumnDetailFramer();
-//
-//            JSONArray jsonArray3 = new JSONArray(jsonUserFramer);
-//            JSONObject jsonObject3 = jsonArray3.getJSONObject(0);
-//            for (int i = 0; i < columnUserFarmer.length; i += 1) {
-//                framerStringArrayList.add(jsonObject3.getString(columnUserFarmer[i]));
-//                Log.d("28AprilV5", "framerStringArrayList[" + i + "] ==> " + framerStringArrayList.get(i));
-//            }
+
 
 
 
